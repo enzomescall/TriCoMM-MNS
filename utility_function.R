bd5e956bdc0b2325cd8fe36045b5aa9fd214b727
enzo <enzo@pop-os.localdomain>
2022-11-12 15:49:46 -0500

    unfcitons

--- utility_function.R
+++ utility_function.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 
 data = read.csv("TriCoMM_DNC_data.csv")
+data$Percent_GreenSpace = data$Percent_GreenSpace/100
+data$PercentPopIncomeBelow2xPovertyLevel = data$PercentPopIncomeBelow2xPovertyLevel/100
 
 G = function(C_max, percent_green) {
   pmax(C_max - percent_green * C_max/60, 0)
@@ -13,7 +15,7 @@ T = function(P, G) {
 }
 
 E = function(T, tree_size, area, percent_green) { # A represents the current % of area covered by greenery 
-  pmin((T*tree_size)/area + percent_green, 90) # cap at 90% green (can be made modular)
+  pmin((T*tree_size)/area + percent_green/100, 0.9) # cap at 90% green (can be made modular)
 }
 
 K = function(B_0, B_1, E) {
@@ -24,7 +26,27 @@ U = function(K, I) {
   K %*% I
 }
 
+mega_function = function(P, C_max, percent_green, tree_size, area, B_0, B_1, I) {
+  G_value = G(C_max, percent_green)
+  print(G_value)
+  T_value = T(P, G_value)
+  print(T_value)
+  E_value = E(T_value, tree_size, area, percent_green)
+  print(E_value)
+  K_value = K(B_0, B_1, E_value)
+  print(K_value)
+  U_value = U(K_value, I)
+  
+  return(U_value)
+}
+
+
 C_max = 100 # cost of breaking in to concrete area
-B_1 = 0.7 # weight given to % greenery to convert it into degrees Celsius (beta derived from regression)
-B_0 = -0.5 # intercept given to % greenery to convert it into degrees Celsius (beta derived from regression)
-n = nrow(data) # size of vectors
+B_0 = -0.2619984 # intercept given to % greenery to convert it into degrees Celsius (beta derived from regression)
+B_1 = 1.48795 # weight given to % greenery to convert it into degrees Celsius (beta derived from regression)
+tree_size = 30
+area = 10000
+
+subset = head(data, 2)
+
+mega_function(100, C_max, subset$Percent_GreenSpace, tree_size, area, B_0, B_1, subset$PercentPopIncomeBelow2xPovertyLevel)
